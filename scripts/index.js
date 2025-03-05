0bd6a9cc070da9c4c4d72244f75c359987ce0319
anna-pervushina <anpervushina@gmail.com>
2025-03-05 19:02:00 +0300

    PR5 done

--- scripts/index.js
+++ scripts/index.js
@@ -9,40 +9,36 @@
 // @todo: Вывести карточки на страницу
 
 // Функция создания карточки
-function createCard(data, cardElement) {
-  const cardTemplate = document.querySelector('#card-template').content.cloneNode(true);
-  
+function createCard(data, handleDelete) {
+  const cardTemplate = document.querySelector("#card-template").content.cloneNode(true);
+
   // Устанавливаем имя и изображение карточки
-  cardTemplate.querySelector('.card__title').textContent = data.name;
-  cardTemplate.querySelector('.card__image').src = data.link;
-  cardTemplate.querySelector('.card__image').alt = data.alt;
+  cardTemplate.querySelector(".card__title").textContent = data.name;
+  cardTemplate.querySelector(".card__image").src = data.link;
+  cardTemplate.querySelector(".card__image").alt = data.alt;
 
   // Добавляем обработчик клика на кнопку удаления
-  cardTemplate.querySelector('.card__delete-button').addEventListener('click', function(event) {
-    const cardElement = event.target.closest('.card'); 
-    if (cardElement) {
-      deleteCard(cardElement);
-    }
-  });
+  cardTemplate.querySelector(".card__delete-button").addEventListener("click", handleDelete);
 
   return cardTemplate;
 }
 
 // Функция удаления карточки
-function deleteCard(cardElement) {
+function deleteCard(event) {
+  const cardElement = event.target.closest(".card");
   cardElement.remove();
 }
 
 // Функция добавления карточек на страницу
-function renderCards(cardsArray, container, cardElement) {
+function renderCards(cardsArray, container, handleDelete) {
   cardsArray.forEach((cardData) => {
-    const newCard = createCard(cardData, cardElement);
-    container.appendChild(newCard);
+      const newCard = createCard(cardData, handleDelete);
+      container.appendChild(newCard);
   });
 }
 
 // Выводим все карточки на страницу
-const cardsContainer = document.querySelector('.places__list');
+const cardsContainer = document.querySelector(".places__list");
 
 // Отображаем существующие карточки
-  renderCards(initialCards, cardsContainer, deleteCard); 
+renderCards(initialCards, cardsContainer, deleteCard);
